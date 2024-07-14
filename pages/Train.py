76e2edaba1453dc0c792f8b26797fe699eb48a50
Soham Shee <83713160+soham-shee@users.noreply.github.com>
2024-07-14 13:49:41 +0530

    Update Train.py

--- pages/Train.py
+++ pages/Train.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('fivethirtyeight')
 import pandas as pd
-import keras
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM, Dropout, GRU, Bidirectional
