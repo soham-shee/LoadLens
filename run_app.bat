104f70570214116d63adb7e6c3e87ccd4be2b7bf
Soham Shee <83713160+soham-shee@users.noreply.github.com>
2025-06-24 14:03:45 +0530

    Update run_app.bat
    debugging.

--- run_app.bat
+++ run_app.bat
@@ -1,14 +1,14 @@
 @echo off
 
-:: Check if virtual environment exists and activate it
-if exist venv (
-    call venv\Scripts\activate
-)
+pip install matplotlib
+pip install pandas
+pip install streamlit
+pip install tensorflow
+pip install keras
+pip install scikit-learn
+pip install pickle4
 
-:: Install dependencies
-pip install -r requirements.txt
-
-:: Run Streamlit app
 streamlit run App.py
+
 
 pause
